6d65855bfced704c57e46138e1e97cb3f8a153a0
Eros Akos <eros.akos@webdream.hu>
2019-04-09 17:01:21 +0200

    Login

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,12 +1,12 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtAuthGuard } from './interfaces/jwt-auth.guard';
-import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class GqlAuthGuard extends JwtAuthGuard {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
+    console.log(context.switchToHttp().getRequest());
     return ctx.getContext().req;
   }
 }
